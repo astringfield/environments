550297df96a47ccbf404dd9515fdb278daabbb45
Josh Thorpe <josh@thorpe.engineering>
2020-03-05 15:08:03 +1100

    updated pytorch jupyter

--- pytorch/pytorch1.4_cuda10.1_jupyter/build.sh
+++ pytorch/pytorch1.4_cuda10.1_jupyter/build.sh
@@ -4,7 +4,3 @@ set -e
 NAME="pytorch1.4_cuda10.1_jupyter"
 echo $0
 docker build -t $NAME .
-# Following line cannot be included in Dockerfile as the '--gpus all' flag is needed for these torch packages
-docker run -it --gpus all --name tmp $NAME pip3 install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric 
-docker commit tmp $NAME
-docker rm tmp
--- pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
+++ pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --gpus '"device=1"'  --rm -p "8888:8888" -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_jupyter
+docker run -it --gpus '"device=0"'  --rm -p "8888:8888" -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_jupyter \
+    jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
