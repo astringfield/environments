cae7443f8e9c5e10799e62ff33724bcec4041376
Josh Thorpe <33478918+ThorpeJosh@users.noreply.github.com>
2020-07-26 18:32:41 +1000

    Update backup_volume.sh

--- jenkins/backup_volume.sh
+++ jenkins/backup_volume.sh
@@ -3,3 +3,7 @@ cd
 mkdir jenkins_home_backup
 docker run --rm -v jenkins_home:/jenkins_home -v /home/jenkins/jenkins_home_backup:/backup ubuntu tar cvjf /backup/jenkins_home.tar.bz2 /jenkins_home
 
+# To exctract
+# In an ubuntu docker image with same mounts as above
+cd /
+tar -xvjf /backup/jenkins_home.tar.bz2
