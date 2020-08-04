368406b2999b22e09d1df370a73f1934b1f38eac
Josh Thorpe <josh@thorpe.engineering>
2020-08-05 01:59:59 +1000

    change user in pytorch container to josh

--- pytorch/pytorch1.5_cuda10.2_jupyter/run.sh
+++ pytorch/pytorch1.5_cuda10.2_jupyter/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 NAME="pytorch1.5_cuda10.2_jupyter"
-docker run -it --gpus '"device=0"'  --rm -p "8888:8888" -p "0.0.0.0:6006:6006" -v /home/josh/repositories/:/repositories $NAME \
+docker run -it --gpus '"device=0"' -u josh --rm -p "8888:8888" -p "0.0.0.0:6006:6006" -v /home/josh/repositories/:/repositories $NAME \
 jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
-#tensorboard --logdir=/repositories/NeuRoRA/tb_logs --bind_all
