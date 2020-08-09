9053f22f5b8def1fc44f6049d4475656a132e3c6
Josh Thorpe <33478918+ThorpeJosh@users.noreply.github.com>
2020-08-09 22:58:09 +1000

    Update backup_volume.sh

--- jenkins/backup_volume.sh
+++ jenkins/backup_volume.sh
@@ -1,2 +1,2 @@
-# As user jenkins to backup volume "jenkins_home"
+# If container named "jenkins" is running then the data from it can be copied as follows
 docker cp jenkins:/var/jenkins_home jenkins_home_volume_backup
