a197f53e9142fd91fb13f007decedf98be62b93a
Josh Thorpe <josh@thorpe.engineering>
2020-10-06 21:30:21 +1100

    Fix SHM and user for pytorch1.4 env

--- pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
+++ pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker run -it --gpus '"device=0"'  --rm -p "8888:8888" -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_jupyter \
+docker run -it \
+    --gpus '"device=0"' \
+    --rm -p "8888:8888" \
+    --shm-size="20G" \
+    -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_jupyter \
+    -u josh
     jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
