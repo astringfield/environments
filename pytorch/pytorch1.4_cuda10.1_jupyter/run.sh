c4406d36a12d1575a4c2a78b25f3c41625c10a9a
Josh Thorpe <josh@thorpe.engineering>
2020-10-25 04:49:21 +1100

    Fix pytorch1.4 jupyter run script

--- pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
+++ pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+NAME="pytorch1.4_cuda10.1_jupyter"
+
 docker run -it \
     --gpus '"device=0"' \
     --rm -p "8888:8888" \
     --shm-size="20G" \
-    -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_jupyter \
-    -u josh
+    -v /home/josh/repositories/:/repositories \
+    -v /etc/localtime:/etc/localtime:ro \
+    -u josh \
+    --name $NAME \
+    $NAME \
     jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
