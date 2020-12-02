da03513dec74d0eb5bab2100157640cf57e6a0b6
Josh Thorpe <josh@thorpe.engineering>
2020-12-03 00:45:43 +1100

    Device and port arguments on run.sh for torch1.4 and 1.7

--- pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
+++ pytorch/pytorch1.4_cuda10.1_jupyter/run.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-NAME="pytorch1.4_cuda10.1_jupyter"
+# First agument is CUDA device(s) number. eg) 1 or 0,3
+if [ -z "$1" ]
+then
+    DEVICES="0"
+    read -rp "Which CUDA devices?:" -e -i "$DEVICES" DEVICES
+else
+    DEVICES="$1"
+fi
 
-docker run -it \
-    --gpus '"device=0"' \
-    --rm -p "8888:8888" \
-    --shm-size="20G" \
-    -v /home/josh/repositories/:/repositories \
-    -v /etc/localtime:/etc/localtime:ro \
-    -u josh \
+# Second argument is port for jupyter
+if [ -z "$2" ]
+then
+    PORT="8888"
+    read -rp "Which port?:" -e -i "$PORT" PORT
+else
+    PORT="$2"
+fi
+
+
+NAME="pytorch1.4_cuda10.1_jupyter"
+# Hostname of container (used in tensorboard filenames)
+HOSTNAME="dock"
+docker run --rm -it --shm-size="20G" \
+    --gpus "\"device=$DEVICES\"" \
     --name $NAME \
+    --hostname $HOSTNAME \
+    -u $USER \
+    -p "$PORT:$PORT" \
+    -v $HOME/repositories/:/repositories \
+    -v /etc/localtime:/etc/localtime:ro \
     $NAME \
-    jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
+    jupyter notebook --port="$PORT" --ip=0.0.0.0 --allow-root --no-browser .
--- pytorch/pytorch1.7_cuda11.0/run.sh
+++ pytorch/pytorch1.7_cuda11.0/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# First agument is CUDA device(s) number. eg) 1 or 0,3
+
+if [ -z "$1" ]
+then
+    echo -n "Which CUDA devices?:"
+    read DEVICES
+else
+    DEVICES="$1"
+fi
+
 # Name for container
 NAME="pytorch1.7_cuda11.0"
 # Hostname of container (used in tensorboard filenames)
@@ -7,9 +17,9 @@ HOSTNAME="dock"
 docker run --rm -it \
     --shm-size="20G" \
     --name $NAME \
-    --gpus '"device=0"' \
+    --gpus "\"device=$DEVICES\"" \
     --hostname $HOSTNAME \
     -v $HOME/repositories:/repositories \
     -v /etc/localtime:/etc/localtime:ro \
-    -u josh \
+    -u $USER \
     $NAME bash
