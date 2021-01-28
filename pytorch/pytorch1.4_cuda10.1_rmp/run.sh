5b02c864c6ad5d90f496a659e4db24210e8d2e04
JoshThorpe <josh@thorpe.engineering>
2021-01-29 00:48:38 +1100

    Removed unused environments

--- pytorch/pytorch1.4_cuda10.1_rmp/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -it --gpus '"device=0"'  --rm -p "8888:8888" -v /home/josh/repositories/:/repositories pytorch1.4_cuda10.1_rmp \
-    jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
