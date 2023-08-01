6be57fb5bd98ca2049a3e9d74ac94ac37745ac89
astringfield <andy.stringfield32@gmail.com>
2023-08-01 10:16:22 +1000

    doc: add instructions for making external venvs/conda-envs visible to jupyterlab once it's up and running

--- jupyter/run_jupyter_lab.sh
+++ jupyter/run_jupyter_lab.sh
@@ -1,2 +1,12 @@
-jupyter lab --port=7000 --notebook-dir /home/astringfield  # for setting up locally
+jupyter lab --port=7000 --notebook-dir ~/  # for setting up locally
 # jupyter lab --ip='10.10.10.19' --port=7000 --notebook-dir /home/astringfield  # for setting up on a remote server
+exit
+
+# If the jupyter_lab environment is newly created, don't forget you need to do the following so that other venv/conda-envs are visible within jupyter-lab
+
+# conda-install `nb_conda_kernels` into the jupyter_lab environment:
+conda install -c conda-forge nb_conda_kernels
+
+# pip-install the following packages into the venv/conda-env you'd like to use 
+# within jupyter
+pip install ipykernel ipywidgets 
